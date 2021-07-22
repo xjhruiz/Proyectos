178b3a49a4b6c19697460a796c72002770898261
Jhonatan Ruiz <59362101+xjhruiz@users.noreply.github.com>
2021-07-22 08:27:09 +0200

    Update Operacion.java

--- java8LambdaStream/src/main/java/com/java8/java8/Operacion.java
+++ java8LambdaStream/src/main/java/com/java8/java8/Operacion.java
@@ -2,7 +2,7 @@ package com.java8.java8;
 
 //INTERFACE FUNCIONAL SOLO UN UNICO METODO ABSTRACTO, PUEDE TENER MAS PERO SOLO UNO ABSTRACTO
 //solamente un método abstracto, es decir puede implementar uno o más métodos default 
-//deberá tener forzosamente un único método abstracto SE puede anotar no anotacion @FunctionalInterface
+//deberá tener forzosamente un único método abstracto SE puede anotar en anotacion @FunctionalInterface
 @FunctionalInterface
 public interface Operacion {
 
